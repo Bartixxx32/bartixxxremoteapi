b1fbb93c92980d8a7ca365c60365346326a32be9
Bartixxx32 <mcbplay1@gmail.com>
2020-01-07 15:49:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+set_time_limit(0);
+
+if (empty($_GET)) {
+    die("No url providen");
+}
+
+$url = $_GET['url'];
+$file = basename($url);
+
+$fp = fopen($file, 'w');
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FILE, $fp);
+
+$data = curl_exec($ch);
+
+curl_close($ch);
+fclose($fp);
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename='.basename($file));
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($file));
+ob_clean();
+flush();
+readfile($file);
+exit;
+?>
